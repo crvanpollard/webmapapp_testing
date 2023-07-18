78fe0f77970a9146283128a0de7182b30883da34
Christopher Pollard <crvanpollard@gmail.com>
2023-07-18 11:59:44 -0400

    fix:conflict

--- Mapbox_Vector_Jsontable/js/script.js
+++ Mapbox_Vector_Jsontable/js/script.js
@@ -1,7 +1,7 @@
 /*********************************************/ 
 /******************* Modal *******************/
 /*********************************************/  
-/*var modal = document.querySelector('#modal')
+var modal = document.querySelector('#modal')
 var modalToggle = document.querySelector('#modal-toggle')
 var close = document.querySelector('#close-modal')
 
@@ -18,9 +18,9 @@ var ariaShowModal = function() {
 }
 
 // open the modal by clicking the div
-modalToggle.onclick = function(){
-  modal.style.display = 'none' ? ariaShowModal() : ariaHideModal()
-}
+// modalToggle.onclick = function(){
+//   modal.style.display = 'none' ? ariaShowModal() : ariaHideModal()
+// }
 
 // closing the modal options: by clicking the 'x' or anywhere outside of it or pressing the escape key
 close.onclick = function(){ariaHideModal()}
@@ -39,7 +39,7 @@ document.onkeydown = function(event) {
     }
   }
 }
-*/
+
 /*********************************************/ 
 /**************** Map ***************/
 /*********************************************/ 
@@ -57,22 +57,12 @@ var tiles = {
     'url': 'https://tiles.dvrpc.org/data/dvrpc-municipal.json'
 }
 
-var census = {
-    'type': 'vector',
-    'url': 'https://tiles.dvrpc.org/data/census_boundaries.json'
-}
-
-var crash = {
-    'type': 'vector',
-    'url': 'https://tiles.dvrpc.org/data/pev.json'
-}
-
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/light-v11',
     attributionControl: false,
-    center: [-77.462261,40.949709 ],
-    zoom: 6
+    center: [-75.2273, 40.071],
+    zoom: 8.5
 });
 
 function generatePopup(popup, e){
@@ -84,18 +74,20 @@ function generatePopup(popup, e){
 
 map.addControl(new mapboxgl.NavigationControl());
 
+
+
 map.on('load', function() {
 
   var layers = map.getStyle().layers;
-    // Find the index of the first symbol layer in the map style
-    var firstSymbolId;
-    for (var i = 0; i < layers.length; i++) {
-    if (layers[i].type === 'line') {
-    firstSymbolId = layers[i].id;
-    break;
-    }
-  }
-/*
+// Find the index of the first symbol layer in the map style
+var firstSymbolId;
+for (var i = 0; i < layers.length; i++) {
+if (layers[i].type === 'line') {
+firstSymbolId = layers[i].id;
+break;
+}
+}
+ // this.map.addLayer(layers.cmp, "water shadow");
   map.addLayer({
     'id': 'county-outline',
     'type': 'line',
@@ -149,108 +141,6 @@ map.on('load', function() {
       ''
     ]
   })
-*/ 
- /* 
-  let expression2 = ["match", ["get", "GEOID10"]]
-  csvPA.forEach(function(row) {
-      let data = row["FutPEV"],
-      color
-    if (data < 10) color = '#eff3ff'
-    else if (data >= 10 && data < 20) color = '#bdd7e7'
-    else if (data >=20 && data < 40) color = '#6baed6'
-    else if (data >= 40 && data < 80) color = '#3182bd'
-    else if (data >= 80 && data < 150) color ='#08519c'
-    else { color = '#08519c'; }
-    expression2.push(row['GEOID10'].toString(), color);
-  });
- // Last value is the default, used where there is no data
-  expression2.push("rgba(0,0,0,0)");
- // map.setPaintProperty('blocks-fill', 'fill-color', expression2)
-
-   map.addLayer({
-    'id': 'blocks-fill',
-    'type': 'fill',
-    'source': census,
-    'source-layer': 'blocks',
-    'layout': {},
-    'paint': {
-      'fill-color': expression2,
-      'fill-opacity': 1
-    }
-  }, "landcover_crop")
-*/
-
-  map.addLayer({
-    'id': 'crash-point',
-    'type': 'circle',
-    'source': crash,
-    'source-layer': 'pev',
-    'paint': {
-      // make circles larger as the user zooms from z12 to z22
-      'circle-radius': {
-      'base': 1.75,
-      'stops': [[5, .75],[11, 2], [22, 180]]
-      },
-      // color circles by ethnicity, using a match expression
-      // https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-match
-      'circle-color': [
-      'match',
-      ['get', 'TYPE'],
-      'current',
-      '#EA563D',
-      'projected',
-      '#3182D1',
-    //  'NJ',
-    //  '#e55e5e',
-     // 2017,
-     // '#3bb2d0',
-      /* other */ '#ccc'
-      ]
-      },
-       "filter": [
-                    "==",
-                    "REGION",
-                    "PA"
-            ],
-                firstSymbolId
-  })
-/*
-  let expression3 = ["match", ["get", "GEOID10"]]
-  csvPA.forEach(function(row) {
-      let data = row["FuPEV_SM"],
-      color
-    if (data < 56.98760) color = '#B6EDF0'
-    else if (data >= 56.98760 && data < 163.153772) color = '#98D2ED'
-    else if (data >=163.153772 && data < 328.378652) color = '#7CBBEB'
-    else if (data >= 328.378652 && data < 602.083758) color = '#5CA3E6'
-    else if (data >= 602.083758 && data < 1013.329840) color ='#368DE3'
-    else if (data >= 1013.329840 && data < 1649.944320) color = '#2176D9'
-    else if (data >=1649.944320 && data < 2692.761851) color = '#2259C7'
-
-    else if (data >= 2692.761851 && data < 4469.331348) color = '#1D3EB5'
-    else if (data >= 4469.331348 && data < 7515.466880) color ='#1727A3'
-    else if (data >= 7515.466880 && data < 12267.415228) color ='#090991'
-    else { color = '#08519c'; }
-    expression3.push(row['GEOID10'].toString(), color);
-  });
- // Last value is the default, used where there is no data
-  expression3.push("rgba(0,0,0,0)");
- // map.setPaintProperty('blocks-fill', 'fill-color', expression2)
-
-   map.addLayer({
-    'id': 'FuPEV SM',
-    'type': 'fill',
-    'source': census,
-    'source-layer': 'blocks',
-    'layout': {},
-    'paint': {
-      'fill-color': expression3,
-      'fill-opacity': 1
-    }
-  }, 
-        firstSymbolId
- // "landcover_crop"
-  )
   
   var popup = new mapboxgl.Popup({
     closebutton: false,
@@ -268,12 +158,12 @@ map.on('load', function() {
     map.setFilter('municipality-hover', ['==', 'geoid', ''])
     popup.remove()
   })
-*/
-  //map.on('click', 'municipality-fill', function(e){
-  //  getReport(e)
- // })
 
- /* let expression = ["match", ["get", "geoid"]]
+  map.on('click', 'municipality-fill', function(e){
+    getReport(e)
+  })
+
+  let expression = ["match", ["get", "geoid"]]
   csvData.forEach(function(row) {
       let data = row["EMIACRE"],
       color
@@ -285,13 +175,13 @@ map.on('load', function() {
     else { color = '#d73027'; }
     expression.push(row['geoid'].toString(), color);
   });
-
+  
   // Last value is the default, used where there is no data
   expression.push("rgba(0,0,0,0)");
   map.setPaintProperty('municipality-fill', 'fill-color', expression)
- */ 
+
 })
 
-//function getReport(e) {
-//  window.open('mcdDetail.aspx?mcdcode='+e.features[0].properties['geoid']);
-//}
+function getReport(e) {
+  window.open('mcdDetail.aspx?mcdcode='+e.features[0].properties['geoid']);
+}
